16f86a9c73873a1289fd4622b61f656ca19bcdd8
Eze-Israel <ezeisraeloluoma123@gmail.com>
2025-05-27 23:53:22 +0100

    mobile login adjustment

---
+++ src/components/Home.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+
+
+const Home = () => {
+  const [showOptions, setShowOptions] = useState(false)
+
+  const handleShow = () => {
+  setShowOptions(true)
+  }
+
+  return (
+  
+    <div>
+      <div >
+      <button onClick={handleShow} className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">Login</button>
+      {showOptions && (
+      <div className='dropdown-options'>
+        <Link href="/Teacher">
+      <button >Login as Teacher</button>
+      </Link>
+      <Link href="/Student">
+      <button>Login as Student</button>
+      </Link>
+      </div>
+   )}
+   </div>
+    
+    </div>
+
+  )
+}
+
+export default Home
